e480e919b166a07b2aa9776e0278cc2860e92419
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-12 19:24:38 +0600

    make-basic-routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function navbar() {
   return (
-    <nav className="bg-white p-5 text-black">
+    <nav className="bg-white p-5 text-black place-items-center">
       <ul className="flex gap-5">
         <li>Home</li>
         <li>Services</li>
