1d97c26d3f2853efcd67a9db78a52ede2dd46115
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-12 19:39:45 +0600

    Link-useRouter-navigation

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  const isLoggedIn = false;
+  const router = useRouter();
+  const handleNavigation = () => {
+    if (isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
+  return (
+    <div>
+      AboutPage <br />
+      <Link href="/about/address">Address</Link>
+      <button
+        className="block cursor-pointer"
+        type="button"
+        onClick={handleNavigation}
+      >
+        Address page
+      </button>
+    </div>
+  );
 }
