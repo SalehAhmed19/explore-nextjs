2a135db90c023e80d82b7e13f1ca474cff50be05
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-12 19:33:28 +0600

    add-link-to-the-navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function navbar() {
   return (
     <nav className="bg-white p-5 text-black place-items-center">
       <ul className="flex gap-5">
-        <li>Home</li>
-        <li>Services</li>
-        <li>About</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/services">Services</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
